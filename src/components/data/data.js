6c86d542230c8ddda6866798b9492d6ef8b6010c
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-04-21 16:39:00 +0300

    new edits

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -68,7 +68,7 @@ export const about = [
     desc: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur",
     desc1:
       "magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor si voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur",
-    cover: "./images/man.png",
+    cover: "../../assets/images/man.png",
   },
 ]
 export const services = [
